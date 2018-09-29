e3c7c7da148112750e4700760659bd4d6d9b41b5
Roman Kunitsyn <roman.kunitsyn@gmail.com>
2018-09-29 16:09:29 +0300

    work-flow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppComponent } from "./app.component";
-import { AppRoutingModule } from "./app-routing.module";
-import { CrudModule } from "./crud/crud.module";
-import { UserModule } from "./user/user.module";
-import { LabModule } from "./lab/lab.module";
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CrudModule } from './crud/crud.module';
+import { UserModule } from './user/user.module';
+import { LabModule } from './lab/lab.module';
 import { NavigationComponent } from './navigation/navigation.component';
 import { HomeComponent } from './home/home.component';
 
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { UserLoginComponent } from "./user-login/user-login.component";
-import { UserLogoutComponent } from "./user-logout/user-logout.component";
-import { UserProfileComponent } from "./user-profile/user-profile.component";
-import { UserRegistrationComponent } from "./user-registration/user-registration.component";
-import { UserRoutingModule } from "./user-routing.module";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { UserLogoutComponent } from './user-logout/user-logout.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
+import { UserRoutingModule } from './user-routing.module';
 
 @NgModule({
   imports: [CommonModule, UserRoutingModule],
